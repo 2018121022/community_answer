ea4497294e8fab6dcf2f1551dbc9bc434cc638ec
2018121022 <vkstk0131@yonsei.ac.kr>
2022-05-22 01:21:28 +0900

    first commit

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vkstk\OneDrive\바탕 화면\Community\myvenv
+set VIRTUAL_ENV=C:\Users\vkstk\OneDrive\바탕 화면\강의자료\Community\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
